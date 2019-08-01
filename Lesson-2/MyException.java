77c732be1b3b92d35077e9e75e41f41ed56064c1
RakBes <RakBes@ambler.ru>
2019-08-01 04:27:11 +0400

    HomeWork-2

---
+++ Lesson-2/MyException.java
@@ -0,0 +1,13 @@
+public class MyException extends RuntimeException {
+
+    private String message;
+
+    public MyException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Exception: " + message;
+    }
+}
